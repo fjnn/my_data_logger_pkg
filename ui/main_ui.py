4f4f5728349bda4baacea286a3b8d0f21e2bb2d6
Gizem Ates <gizemates@iyte.edu.tr>
2020-04-19 16:00:46 +0200

    roll figs added

--- ui/main_ui.py
+++ ui/main_ui.py
@@ -90,7 +90,7 @@ if __name__ == "__main__":
     import sys
     pic_list = [0,1,2,3,4,5,6,5,4,3,2,1,0,7,8,9,10,11,12,11,10,9,8,7]
     pic_index = 0
-    motion_index = 0;
+    motion_index = 0
     app = QtGui.QApplication(sys.argv)
     Form = QtGui.QWidget()
     ui = Ui_Form()
