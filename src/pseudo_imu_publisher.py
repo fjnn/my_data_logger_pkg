0f2e60fd6d6c465cfc85639465ceffb29f112a46
Gizem Ates <gizemates@iyte.edu.tr>
2020-05-22 14:14:07 +0200

    second imu is added

--- src/pseudo_imu_publisher.py
+++ src/pseudo_imu_publisher.py
@@ -6,7 +6,7 @@ Publish a pseudo IMU data like a bag file.
 """
 
 import rospy
-from std_msgs.msg import Header
+# from std_msgs.msg import Header
 from std_msgs.msg import String
 from sensor_msgs.msg import Imu
 
@@ -23,30 +23,50 @@ def talker():
 
 
 def pseudo_imu_msg_creator():
-    pub_imu = rospy.Publisher('/pseudo_imu_msg', Imu, queue_size=10)
+    pub_imu1 = rospy.Publisher('/pseudo_imu1_msg', Imu, queue_size=10)
+    pub_imu2 = rospy.Publisher('/pseudo_imu2_msg', Imu, queue_size=10)
     rospy.init_node('pseudo_imu_msg_creator', anonymous=False)
     rate = rospy.Rate(10)  # 10hz
-    imu_data = Imu()
+    imu1_data = Imu()
+    imu2_data = Imu()
     index = 0.0
     while not rospy.is_shutdown():
         # imu_data.header = Header()
-        imu_data.header.stamp = rospy.Time.now()
-        imu_data.header.frame_id = "imu_data"
-        imu_data.orientation.x = 0.0
-        imu_data.orientation.y = 0.0
-        imu_data.orientation.z = 0.0
-        imu_data.orientation.w = 1.0
-        imu_data.orientation_covariance[0] = -1.0
-        imu_data.angular_velocity.x = index
-        imu_data.angular_velocity.y = 0.0
-        imu_data.angular_velocity.z = 0.0
+        imu1_data.header.stamp = rospy.Time.now()
+        imu1_data.header.frame_id = "imu1_data"
+        imu1_data.orientation.x = 0.0
+        imu1_data.orientation.y = 0.0
+        imu1_data.orientation.z = 0.0
+        imu1_data.orientation.w = 1.0
+        imu1_data.orientation_covariance[0] = -1.0
+        imu1_data.angular_velocity.x = 10.0
+        imu1_data.angular_velocity.y = 0.0
+        imu1_data.angular_velocity.z = 0.0
         # imu_data.angular_velocity_covariance = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        imu_data.linear_acceleration.x = 0.0
-        imu_data.linear_acceleration.y = 0.0
-        imu_data.linear_acceleration.z = 0.0
+        imu1_data.linear_acceleration.x = 0.0
+        imu1_data.linear_acceleration.y = 0.0
+        imu1_data.linear_acceleration.z = 0.0
         # imu_data.linear_acceleration_covariance = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
-        rospy.loginfo(imu_data)
-        pub_imu.publish(imu_data)
+
+        imu2_data.header.stamp = rospy.Time.now()
+        imu2_data.header.frame_id = "imu2_data"
+        imu2_data.orientation.x = 0.0
+        imu2_data.orientation.y = 0.0
+        imu2_data.orientation.z = 0.0
+        imu2_data.orientation.w = 1.0
+        imu2_data.orientation_covariance[0] = -1.0
+        imu2_data.angular_velocity.x = 20.0
+        imu2_data.angular_velocity.y = 0.0
+        imu2_data.angular_velocity.z = 0.0
+        # imu_data.angular_velocity_covariance = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+        imu2_data.linear_acceleration.x = 0.0
+        imu2_data.linear_acceleration.y = 0.0
+        imu2_data.linear_acceleration.z = 0.0
+        # imu_data.linear_acceleration_covariance = [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]
+
+        rospy.loginfo("started")
+        pub_imu1.publish(imu1_data)
+        pub_imu2.publish(imu2_data)
         rate.sleep()
         index = 0 if index >= 250.0 else index + 0.5
 
