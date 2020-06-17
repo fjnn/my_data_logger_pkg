1623888e82fdccff0f25f6a133fd15e29d2a148e
Gizem Ates <gizemates@iyte.edu.tr>
2020-06-17 11:25:14 +0200

    r.sleep() unutmuşum :D

--- src/data_logger_node.py
+++ src/data_logger_node.py
@@ -9,16 +9,17 @@ import rospy
 from Classes.IMU_subscriber_class import IMUdataRecorder
 # from Classes.UI_form import UI_form
 
-
-if __name__ == "__main__":
-    ros_node = IMUdataRecorder()
-    while not ros_node.runflag:
-        ros_node.update()
-
-# TO RUN MANUAL
+# TO RUN FROM GUI
 # if __name__ == "__main__":
 #     ros_node = IMUdataRecorder()
-#     ros_node.init_subscribers_and_publishers()
-#     # while not ros_node.runflag:
-#     while not rospy.is_shutdown():
+#     while not ros_node.runflag:
 #         ros_node.update()
+
+# TO RUN MANUAL
+if __name__ == "__main__":
+    ros_node = IMUdataRecorder()
+    ros_node.init_subscribers_and_publishers()
+    # while not ros_node.runflag:
+    while not rospy.is_shutdown():
+        ros_node.update()
+        ros_node.r.sleep()
